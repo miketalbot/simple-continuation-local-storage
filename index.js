5a5430e370fad043c69c9be658158f4435133e85
Michael Talbot <mike.talbot@alcumusgroup.com>
2020-05-29 00:01:45 +0100

    build

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ module.exports = new Proxy( getCurrent, {
             current[HOLD] = true
             if(inAsync) {
                 current._parent[HOLD] = true
+                current._parent[HOLD]._parent[HOLD] = true
             }
         }
         if( prop === '$') return current
